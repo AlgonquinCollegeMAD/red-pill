250d49a59db0730c0fedde3ed86b055ba54c1a6c
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-12-03 21:15:31 -0500

    Add imageURL in model to store the URL of an image associated with a post

--- RedPill/Model/Post.swift
+++ RedPill/Model/Post.swift
@@ -9,8 +9,9 @@ import Foundation
 import FirebaseFirestore
 
 struct Post: Codable, Identifiable {
-  @DocumentID var id: String?
-  var content: String
-  var authorID: String
-  var timestamp: Date
+    @DocumentID var id: String?
+    var content: String
+    var authorID: String
+    var timestamp: Date
+    var imageURL: URL?
 }
