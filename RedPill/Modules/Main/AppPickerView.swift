fd1ab74419a03ce8b79f73604abd463daebb1f77
JanineAnn Buenaseda <buen0024@algonquinlive.com>
2024-12-03 21:17:05 -0500

    Add PickerView file

---
+++ RedPill/Modules/Main/AppPickerView.swift
@@ -0,0 +1,37 @@
+//
+//  AppPickerView.swift
+//  RedPill
+//
+//  Created by Janine Ann Buenaseda on 2024-12-03.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct AppPickerView: View {
+  @Binding var selectedImage: UIImage?
+  @State private var selectedPickerItem: PhotosPickerItem?
+  
+  var body: some View {
+    PhotosPicker("Pick a Photo", selection: $selectedPickerItem, matching: .images)
+      .onChange(of: selectedPickerItem) { oldValue, newValue in
+        Task {
+          await loadImage(from: newValue)
+        }
+      }
+    
+  }
+  
+  private func loadImage(from pickerItem: PhotosPickerItem?) async {
+    guard let pickerItem = pickerItem else { return }
+    
+    do {
+      if let data = try await pickerItem.loadTransferable(type: Data.self),
+         let image = UIImage(data: data) {
+        selectedImage = image
+      }
+    } catch {
+      print("Failed to load image: \(error.localizedDescription)")
+    }
+  }
+}
